eb2ba5f953490c57215c7bea9975b5294d6ab68d
Artem Kalach <rtmklcwork@gmail.com>
2022-07-09 20:13:14 +0300

    3d comm

--- src/components/Prop.js
+++ src/components/Prop.js
@@ -1,10 +1,71 @@
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
-const Prop = (props) =>{
-    return(
+const Prop = (props) => {
+    return (
         <div>
-            <h1>{`Props (Пропсы)`}</h1>
+            <div>
+                <h1>{`Props (Пропсы)`}</h1>
+            </div>
+            <div>
+                <h2>То, что мы называем аргументами в мире функций, в мире компонентов называется свойствами. Функциональные компоненты свойства принимают в качестве аргумента.
+                    Props представляют собой объект, содержащие свойства.
+                    Обратиться к ним можно - props.имя-свойства.
+                    Props - только для чтения.
+                </h2>
+            </div>
+            <div>
+                {` function Welcome(props) {
+                return <h1>Привет, {props.name}</h1>;
+            }`}
+            </div>
+            <div>
+                <h2>Классовые компоненты свойства принимают по умолчанию и обратиться к ним можно
+                    с помощью this.props.имя-свойства.
+                </h2>
+            </div>
+            <div>
+                {` class Welcome extends React.Component {
+                render() {
+                    return <h1>Привет, {this.props.name}</h1>;
+                }
+            }`}
+            </div>
+            <div>
+                <h2>Элементы могут описывать и наши собственные компоненты:</h2>
+            </div>
+            <div>
+                {`  const element = <Welcome name="Алиса" />;`}
+            </div>
+            <div>
+                <h2>
+                    Когда React встречает подобный элемент, он собирает все JSX-атрибуты и дочерние элементы в один объект
+                    и передаёт их нашему компоненту. Этот объект называется «пропсы».
+
+                    Например, этот компонент выведет «Привет, Алиса» на страницу:
+                </h2>
+            </div>
+            <div>
+                {` function Welcome (props) {
+                return <h1> Привет, {props.name}</h1>;
+            }
+
+            const element = <Welcome name="Алиса" />;
+            ReactDOM.render(
+                element,
+                document.getElementById('root')
+            );`}
+            </div>
+            <div>
+                <h2>
+                Мы передаём React-элемент {`<Welcome name="Алиса">`} в ReactDOM.render().
+React вызывает наш компонент Welcome с пропсами {`{name: 'Алиса'}`}.
+Наш компонент Welcome возвращает элемент {`<h1>Привет, Алиса</h1>`} в качестве результата.
+React DOM делает минимальные изменения в DOM, чтобы получилось {`<h1>Привет, Алиса</h1>`}.
+
+Компонент никогда не должен что-то записывать в свои пропсы— вне зависимости от того, функциональный он или классовый.
+                </h2>
+            </div>
         </div>
     )
 }
--- src/components/State.js
+++ src/components/State.js
@@ -1,11 +1,82 @@
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+import "./State.css"
 
-const State1 = (props) =>{
-    return(
+const State1 = (props) => {
+    const [count, setCount] = useState(0)
+    return (
         <div>
-            <h1>State</h1>
+            <div>
+                <h1>Состояние</h1>
+            </div>
+            <div>
+                <span>
+                    State (состояние) в React – это объект простого JS, позволяющий отслеживать
+                    данные компонента. Состояние компонента может меняться. Смена состояния компонента зависит от функциональности
+                    приложения. Изменения могут основываться на ответе от пользователя, новых сообщениях с сервера, ответа сети и т.д.
+                    Состояние компонента должно быть приватным для компонента и контролироваться им.
+                    Изменения состояния компонента необходимо делать внутри компонента – инициализация и обновление состояния компонента.
+                </span>
+            </div>
+            <div>
+                <h2>
+                {`Class App extends React.Component{
+                    constructor(props){
+                        super(props)
+                        this.state = {username : "John Gold"}
+                    }
+                    render(){
+                        return(
+                            <div>{this.state.username}</div>
+                        )
+                    }
+                }`}
+                </h2>
+            </div>
+            <div>
+                <p>Единственный допустимый способ обновления состояния компонента – через setState().</p>
+                <p>Так нельзя менять состояние: this.state.username='Mark'</p>
+                <p>1й способ изменить state:</p>
+            </div>
+            <div>
+                <h2>
+                {`this.setState({userName: "Artem"})`}
+                </h2>
+            </div>
+            <div>
+                <p>2й способо изменить state:</p>
+            </div>
+            <div>
+                <h2>
+                    {`this.setState(()=>({username : "Artem"}))`}
+                </h2>
+            </div>
+            <div>
+                <p>В функциональных компонентах state создается с помощью хука useState()</p>
+                <p>Чтобы воспользоваться им, необходимо импортировать useState из 'react':</p>
+            </div>
+            <div>
+                <h2>
+                    {`import {useState} from 'react'`}
+                </h2>
+            </div>
+            <div>
+                <p>
+                Хук useState() принимается в качестве параметра первоначальное значение для переменной,
+                 а возвращает массив, содержащий переменную и функцию, которая изменяет эту переменную.
+                </p>
+            </div>
+            <div>
+                <p>Примером работы со state является счётчик, который изменяется при нажатии на кнопку:</p>
+            </div>
+            <div className="count">
+                    <button className="countBtn" onClick={()=>setCount(count+1)}>Кнопка +</button>
+                    <p>Счетчик:{count}</p>
+                    <button className="countBtn" onClick={()=>setCount(count-1)}>Кнопка -</button>
+            </div>
+
         </div>
+
     )
 }
 
